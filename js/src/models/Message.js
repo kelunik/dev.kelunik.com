83e08cb2164aec68cdbc7d039ab83b45b71a11c8
Niklas Keller <me@kelunik.com>
2015-05-17 18:26:57 +0200

    Replace pizza with amphp logo

--- js/src/models/Message.js
+++ js/src/models/Message.js
@@ -6,6 +6,7 @@ module.exports = Backbone.Model.extend({
     defaults: {
         authorId: 0,
         authorName: "System",
+        authorAvatar: "https://avatars1.githubusercontent.com/u/8865682?v=3&s=200",
         text: "",
         html: "",
         createdAt: 0
