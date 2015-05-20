acc72cddc52d66b33d448f7713e9751beafed5b6
Niklas Keller <me@kelunik.com>
2015-05-20 22:19:40 +0200

    Add ability to use tabs for indents in textarea

--- src/js/views/InputView.js
+++ src/js/views/InputView.js
@@ -50,12 +50,58 @@ module.exports = Backbone.View.extend({
 
                 return false;
             }
+        } else if (event.which === 9) {
+            return this.onTab(event);
         }
     },
 
     onSend: function (event) {
         event.preventDefault();
         this.submit();
+
+        return false;
+    },
+
+    onTab: function (event) {
+        event.preventDefault();
+
+        var start = this.input.selectionStart;
+        var end = this.input.selectionEnd;
+        var value = this.input.value;
+        var before = value.substr(0, start);
+        var after = value.substr(end);
+
+        if (start === end) {
+            this.input.value = before + "\t" + after;
+            this.input.selectionStart = this.input.selectionEnd = start + 1;
+        } else {
+            var selectStart = start;
+
+            var line_before = before.substr(0, Math.max(0, before.lastIndexOf("\n") + 1));
+            var line_after = after.substr(Math.max(0, after.indexOf("\n")));
+            var text_to_indent = before.substr(Math.max(0, before.lastIndexOf("\n") + 1))
+                + value.substring(start, end)
+                + after.substr(0, Math.max(0, after.indexOf("\n")));
+
+            if (event && event.shiftKey) {
+                selectStart -= /(^|\n)(\t| {0,4})/g.exec(text_to_indent)[2].length;
+
+                text_to_indent = text_to_indent.replace(/(^|\n)(\t| {0,4})/g, "\n");
+
+                if (text_to_indent.indexOf("\n") === 0) { // TODO: Just get first char and compare
+                    text_to_indent = text_to_indent.substr(1);
+                }
+
+                this.input.value = line_before + text_to_indent + line_after;
+            } else {
+                selectStart++;
+
+                this.input.value = line_before + "\t" + text_to_indent.replace(/\n/g, "\n\t") + line_after;
+            }
+
+            this.input.selectionStart = selectStart;
+            this.input.selectionEnd = this.input.value.length - after.length;
+        }
 
         return false;
     },
