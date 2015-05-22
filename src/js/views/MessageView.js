9b82511b1ae2f59dd9cca960ef90966793d9a7ea
Niklas Keller <me@kelunik.com>
2015-05-22 13:47:39 +0200

    Message author overflow → ..., attributes @ MessageView

--- src/js/views/MessageView.js
+++ src/js/views/MessageView.js
@@ -6,14 +6,16 @@ module.exports = Backbone.View.extend({
     tagName: "div",
     className: "message",
     template: require("../templates/message.handlebars"),
+    attributes: {
+        tabindex: "0"
+    },
 
     initialize: function () {
-        
+
     },
 
     render: function () {
         this.$el.html(this.template(this.model.toJSON()));
-        this.$el.attr("tabindex", "0");
 
         return this;
     }
